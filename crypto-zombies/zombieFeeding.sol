e4f75c8a0ed0b70630f3ce7a0a7843701a19151e
Steve B <bedell.369@osu.edu>
2019-02-05 16:19:27 -0500

    Updating CryptoZombies files - finished tutorial through Lesson 5

--- crypto-zombies/zombieFeeding.sol
+++ crypto-zombies/zombieFeeding.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.25;
 
-import "./zombiefactory.sol";
+import "./zombieFactory.sol";
 
 contract KittyInterface {
   function getKitty(uint256 _id) external view returns (
@@ -21,7 +21,7 @@ contract ZombieFeeding is ZombieFactory {
 
   KittyInterface kittyContract;
 
-  modifier ownerOf(uint _zombieId) {
+  modifier onlyOwnerOf(uint _zombieId) {
     require(msg.sender == zombieToOwner[_zombieId]);
     _;
   }
@@ -38,7 +38,7 @@ contract ZombieFeeding is ZombieFactory {
       return (_zombie.readyTime <= now);
   }
 
-  function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) internal ownerOf(_zombieId) {
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) internal onlyOwnerOf(_zombieId) {
     Zombie storage myZombie = zombies[_zombieId];
     require(_isReady(myZombie));
     _targetDna = _targetDna % dnaModulus;
